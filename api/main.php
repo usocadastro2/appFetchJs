5246d0d641de17809b447dfa57bed3fb8b4b3631
Ricardo <usocadastro2@outlook.com>
2025-03-26 11:52:47 -0300

    Atualização para vercel

--- api/main.php
+++ api/main.php
@@ -4,19 +4,25 @@
     header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
     header('Access-Control-Allow-Headers: Content-Type');
 
-    $banco = [
-        ["nome" => "Ostra", "email" => "ostra@outlook.com"],
-        ["nome" => "Ana", "email" => "dark@yahoo.com"],
-        ["nome" => "Pirata", "email" => "altomar@terra.com.br"]
-    ];
+    if($_SERVER['REQUEST_METHOD']=== 'POST'){
+        $banco = [
+            ["nome" => "Ostra", "email" => "ostra@outlook.com"],
+            ["nome" => "Ana", "email" => "dark@yahoo.com"],
+            ["nome" => "Pirata", "email" => "altomar@terra.com.br"]
+        ];
 
-    $dados = json_decode(file_get_contents('php://input'), true);
-    
+        $dados = json_decode(file_get_contents('php://input'), true);
+        
 
-    if(in_array('sucesso', $dados)){
-        echo json_encode($banco);
+        if(in_array('sucesso', $dados)){
+            echo json_encode($banco);
+        }else{
+            echo json_encode("Nada feito");
+        };
     }else{
-        echo json_encode("Nada feito");
+        http_response_code(405);
+        echo json_encode(["error" => "Méthodo não permitido"]);
+
     };
 
 ?>
