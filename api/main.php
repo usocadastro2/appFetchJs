958af106e328d4dc7440c3a42c4950f9bc6e0606
Ricardo <usocadastro2@outlook.com>
2025-03-26 13:16:18 -0300

    Ainda Vercel

--- api/main.php
+++ api/main.php
@@ -1,10 +1,10 @@
 <?php
     header('Content-Type: application/json');
     header('Access-Control-Allow-Origin: *');
-    header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+    header('Access-Control-Allow-Methods: POST');
     header('Access-Control-Allow-Headers: Content-Type');
 
-    if($_SERVER['REQUEST_METHOD']=== 'POST'){
+    if($_SERVER['REQUEST_METHOD' === 'POST']){
         $banco = [
             ["nome" => "Ostra", "email" => "ostra@outlook.com"],
             ["nome" => "Ana", "email" => "dark@yahoo.com"],
@@ -14,15 +14,16 @@
         $dados = json_decode(file_get_contents('php://input'), true);
         
 
-        if(in_array('sucesso', $dados)){
+        if(in_array('sucesso', $dados['status'])){
             echo json_encode($banco);
         }else{
             echo json_encode("Nada feito");
         };
     }else{
         http_response_code(405);
-        echo json_encode(["error" => "Méthodo não permitido"]);
-
-    };
+        echo json_encode(["error" => "Método não é POST"]);
+        exit;
+    }
+    
 
 ?>
